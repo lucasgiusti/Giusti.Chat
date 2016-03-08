ba7ce57b66a789861600d343faf62093573f6067
Lucas Giusti Pereira <giusti.lucas@gmail.com>
2016-03-07 22:02:55 -0300

    excluindo arquivos do projeto node.

--- Giusti.Chat.Node/server.js
+++ Giusti.Chat.Node/server.js
@@ -11,7 +11,7 @@
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 //************************************************************
 
 io.on('connection', function () {
